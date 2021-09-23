042c1f93378ab74653ce573da468e14a0a2d5a48
Alexey Barashkov <barasher@yandex.ru>
2021-09-23 10:38:14 +0300

    Make initial UKF implementation

--- src/ukf.h
+++ src/ukf.h
@@ -20,7 +20,7 @@ class UKF {
    * ProcessMeasurement
    * @param meas_package The latest measurement data of either radar or laser
    */
-  void ProcessMeasurement(MeasurementPackage meas_package);
+  void ProcessMeasurement(const MeasurementPackage& meas_package);
 
   /**
    * Prediction Predicts sigma points, the state, and the state covariance
@@ -33,14 +33,25 @@ class UKF {
    * Updates the state and the state covariance matrix using a laser measurement
    * @param meas_package The measurement at k+1
    */
-  void UpdateLidar(MeasurementPackage meas_package);
+  void UpdateLidar(const MeasurementPackage& meas_package);
 
   /**
    * Updates the state and the state covariance matrix using a radar measurement
    * @param meas_package The measurement at k+1
    */
-  void UpdateRadar(MeasurementPackage meas_package);
+  void UpdateRadar(const MeasurementPackage& meas_package);
 
+  /**
+   * Returns current estimated state vector
+   * @return current state vector
+   */
+  const Eigen::VectorXd& GetX() const
+  {
+      return x_;
+  }
+
+private:
+  void UpdateCommon(const Eigen::VectorXd& z, const Eigen::MatrixXd& Zsig, const Eigen::MatrixXd& R);
 
   // initially set to false, set to true in first call of ProcessMeasurement
   bool is_initialized_;
